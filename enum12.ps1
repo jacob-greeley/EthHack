c68d6d3b319cfa6014eebda412990b9a6b42b1df
Jacob Greeley <jacob.greeley@mymail.champlain.edu>
2026-09-15 12:40:01 -0400

    Update enum12.ps1
    fixed outfile

--- enum12.ps1
+++ enum12.ps1
@@ -1,46 +1,53 @@
 #This script will run 12 commands listing a bunch of important information about the machine
 
+#defines the output folder
+$Outfolder = "C:\temp"
+
+#if the outputfolder does not exist
+if (!(Test-Path -Path $OutFolder)) {
+    New-Item -ItemType Directory -Path $OutFolder
+}
 #lists current user, groups, and privileges
 
-whoami /all > whoiseveryone.txt
+whoami /all | Out-File $OutFolder\whoiseveryone.txt
 
 #Lists running processes
-tasklist /v > processes.txt
+tasklist /v | Out-File $OutFolder\processes.txt
 
 #Lists any open network sockets and connections 
 
-netstat -ano > opensocksandconnections.txt
+netstat -ano | Out-File $OutFolder\opensocksandconnections.txt
 
 #Lists running services
-net start > services.txt
+net start | Out-File $OutFolder\ervices.txt
 
 #lists all local user accounts
-net user > alllxtocalusers.txt
+net user | Out-File $OutFolder\alllxtocalusers.txt
 
 #lists all local groups
-net localgroup > alllocalgroups.txt
+net localgroup | Out-File $OutFolder\alllocalgroups.txt
 
 #lists all users in administrators groups
 
-net localgroup administrators > allusersinadmin.txt
+net localgroup administrators | Out-File $OutFolder\allusersinadmin.txt
 
 #Shows the network configuration like Ip, gateway, and DNS
 
-ipconfig /all > netconfig.txt
+ipconfig /all | Out-File $OutFolder\ netconfig.txt
 
 #shows all firewall polices
 
-netsh advfirewall firewall show rule name=all > firwallpolices.txt
+netsh advfirewall firewall show rule name=all | Out-File $OutFolder\firwallpolices.txt
 
 #Shows the ARP table
 
-arp -a > arptable.txt
+arp -a | Out-File $OutFolder\arptable.txt
 
 #shows any active SMB sessions
 
-net use > active smb sessions
+net use | Out-File $OutFolder\activesmbsessions.txt
 
 #Shows system information
 
-systeminfo > systeminfo.txt
+systeminfo | Out-File $OutFolder\systeminfo.txt
 
